ab3a3287f204dfad708f9c653fa7922da003e5cb
luem2 <lucianoepinol@gmail.com>
2026-08-02 23:31:53 -0300

    fix wrap diffview & open new terminal (without herdr multiplexer)

--- configs/nvim/lua/plugins/git.lua
+++ configs/nvim/lua/plugins/git.lua
@@ -6,6 +6,15 @@ return {
         default = { disable_diagnostics = true },
         file_history = { disable_diagnostics = true },
       },
+      hooks = {
+        diff_buf_win_enter = function()
+          vim.opt_local.wrap = true
+          vim.opt_local.linebreak = true
+          vim.opt_local.breakindent = true
+          vim.opt_local.smoothscroll = true
+          vim.opt_local.showbreak = "↳ "
+        end,
+      },
     },
     keys = {
       { "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Open diff view" },
