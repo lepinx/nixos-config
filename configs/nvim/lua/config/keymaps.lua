df506965ac4b8320c43f8d0c6065e31ff4b73730
luem2 <lucianoepinol@gmail.com>
2026-08-04 20:25:08 -0300

    remove problematic keymap

--- configs/nvim/lua/config/keymaps.lua
+++ configs/nvim/lua/config/keymaps.lua
@@ -2,4 +2,3 @@ local map = vim.keymap.set
 
 map("n", "<leader>[", "zc", { desc = "Close fold" })
 map("n", "<leader>]", "zo", { desc = "Open fold" })
-map("n", ";i", "<cmd>Inspect<CR>", { desc = "Inspect highlight" })
